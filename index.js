90377f4eec43e54a3652b9935e6b6df6e9850569
Fahmi Fahreza <78774559+Man4ct@users.noreply.github.com>
2022-06-30 13:20:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,16 @@
  * 8
  */
 let numbers = [3, 1, 2, 3, 7, 5, 6, 8, 2, 1];
+let max = 0;
 
 function result(numbers) {
-    // Your Code Here
-return Math.max(...numbers);
-}
+    for (let i = 0; i < numbers.length; i++){
+        if (numbers[i] > max){
+        max = numbers[i];
+        }
+    }
+    console.log(max);
+    }
 
-console.log(result(numbers));
-
-
+result(numbers);
 
